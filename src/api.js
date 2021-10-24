3632b942e05f85dc89c9dbe47ccb599f7696cf22
Marina Blin <m4rina.blin@gmail.com>
2021-10-24 17:58:42 +0200

    :sparkles:Ajout du profil

--- src/api.js
+++ src/api.js
@@ -9,38 +9,59 @@ export const client = new ApolloClient({
   headers: { Authorization: 'Bearer ' + SECRET.TOKEN }
 });
 
-export const allProject =
-    gql`
-    {
-        repositoryOwner(login:"Bugsyaya") {
-            repositories(first:50, affiliations:OWNER) {
-                nodes{
-                    updatedAt,
-                    createdAt,
-                    languages(first:10) {
-                        nodes{
-                            name
-                        }
-                    },
-                    description,
-                    forkCount,
-                    stargazerCount,
-                    name,
-                    url,
-                    repositoryTopics(first:10) {
-                        edges{
-                            node{
-                                topic{
-                                    name
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+export const user =
+gql`
+  query { 
+    user(login:"Bugsyaya") {
+      id
+      name
+      createdAt
+      updatedAt
+      company
+      followers {
+        totalCount
+      }
+      following {
+        totalCount
+      }
+      bio
+      avatarUrl
     }
-    `
+  }
+`
+
+export const allProject =
+gql`
+query {
+  repositoryOwner(login:"Bugsyaya") {
+    repositories(first:50, affiliations:OWNER) {
+      nodes{
+        updatedAt,
+        createdAt,
+        languages(first:10) {
+          nodes{
+            name
+          }
+        },
+        description,
+        forkCount,
+        stargazerCount,
+        name,
+        url,
+        repositoryTopics(first:10) {
+          edges{
+            node{
+              topic{
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export const byProjectName =
 gql`
