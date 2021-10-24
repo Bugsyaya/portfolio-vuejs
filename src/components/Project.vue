d0a50223e583971e3af23f18688f936d1bfdd115
Marina Blin <m4rina.blin@gmail.com>
2021-10-24 17:46:58 +0200

    :sparkles:Ajout de apollo et de l'API

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,66 +1,24 @@
 <template>
   <div id="project">
-    <h1>{{project.name}} ({{project.id}})</h1>
-    <PieChart ref="skills_chart" :data="chartData" :options="chartOptions" ></PieChart>
+    <h1>{{repositoryOwner.repository.name}} ({{repositoryOwner.repository.id}})</h1>
   </div>
 </template>
 
 <script>
 import * as api from '../api'
-import PieChart from "./PieChart.vue"
-import randomColor from 'randomcolor'
 
 export default {
   name: 'Project',
-  components: {
-      PieChart
-  },
-  data () {
-    return {
-        project: {},
-        chartOptions: {
-            hoverBorderWidth: 20
-        },
-        chartData: {
-            hoverBackgroundColor: "red",
-            hoverBorderWidth: 10,
-            labels: [],
-            datasets: [
-            {
-                label: "Data One",
-                backgroundColor: [],
-                data: []
-            }
-            ]
+  apollo: {
+      repositoryOwner() {
+        return {
+          query:api.byProjectName,
+          variables: {
+            name: this.$route.params.name,
+          },
         }
-    }
+      }
   },
-  created () {
-        this.getProjectByName(),
-        this.getProjectLanguageByName()
-  },
-  computed: {
-    currentDataSet () {
-      return this.chartData.datasets[0].data
-    }
-  },
-  methods : {
-        getProjectByName(){
-            api.getProjectByName(this.$route.params.name).then(response => {
-                this.project = response.data
-            })
-        },
-        async getProjectLanguageByName() {
-            const result = await api.getProjectLanguageByName(this.$route.params.name)
-
-            console.log(result.data)
-
-            this.chartData.labels = Object.keys(result.data)
-            this.chartData.datasets[0].data = Object.values(result.data)
-            this.chartData.datasets[0].backgroundColor = Object.keys(result.data).map(() => randomColor())
-            this.$refs.skills_chart.update();
-        }
-  }
 }
 </script>
 
